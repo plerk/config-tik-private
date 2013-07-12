74c6401b9b66ad61ec0f1193c522c11013b90a85
Graham Ollis <gollis@sesda3.com>
2013-07-12 16:18:05 -0400

    better sync

--- sync.sh
+++ sync.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+cd ~/etc/private
+git pull origin master
 rsync --delete -avH pac:/mnt/nasa/local/ /usr/local/
 rsync --delete -avH pac:/mnt/nasa/util/ /util/
-
+mkdir -p ~/.ssh
+chmod 700 ~/.ssh
 cp ~/etc/private/ssh_config ~/.ssh/config
 chmod 644 ~/.ssh/config
 
